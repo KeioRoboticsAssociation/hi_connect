07fc91901a73ed599816fb50f4f24c58b64c9dac
hirobon1690 <hirobon1690@gmail.com>
2023-08-21 19:13:17 +0900

    FIX: データ型をアプデ

--- hi_connect/connect.py
+++ hi_connect/connect.py
@@ -2,16 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from typing import List
-from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import Float32
-from std_msgs.msg import Int8MultiArray
-from std_msgs.msg import Int16MultiArray
-from std_msgs.msg import Int32MultiArray
-from sensor_msgs.msg import JointState
-from std_msgs.msg import Int8
-from std_msgs.msg import Bool
-from std_msgs.msg import Empty
-from std_msgs.msg import Header
+from catch2023_interfaces.msg import CreateMessage
 import serial
 import math
 import serial.tools.list_ports
@@ -20,7 +11,7 @@ import serial.tools.list_ports
 class Connect(Node):
     def __init__(self):
         super().__init__('connect')
-        self.subscription = self.create_subscription(Float32MultiArray, 'degpos_data', self.deg_callback, 10)
+        self.subscription = self.create_subscription(CreateMessage, 'degpos_data', self.deg_callback, 10)
         self.port = serial.tools.list_ports.comports()[0].device
         print(self.port)
         self.uart = serial.Serial(self.port, 115200)
@@ -28,12 +19,13 @@ class Connect(Node):
         self.tmr = self.create_timer(0.00001, self.callback)
 
     def send(self):
-        message = str(self.deg[0])+','+str(self.deg[1])+'\n'
+        message = str(self.deg[0])+','+str(self.deg[1])+str(self.stepper)+'\n'
         self.uart.write(message.encode('ascii'))
 
     def deg_callback(self, deg_msg):
-        self.deg[0] = math.floor(deg_msg.data[0]*100)/100
-        self.deg[1] = math.floor(deg_msg.data[1]*100)/100
+        self.deg[0] = math.floor(deg_msg.data.r*100)/100
+        self.deg[1] = math.floor(deg_msg.data.theta*100)/100
+        self.stepper = deg_msg.data.stepper
 
     def receive(self):
         line = self.uart.readline()
