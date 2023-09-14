ab11d43af81da6073c7a7d464d0dd4c08b42cbe0
hirobon1690 <hirobon1690@gmail.com>
2023-09-14 11:51:53 +0900

    ADD: guriguri

--- hi_connect/connect_seiton.py
+++ hi_connect/connect_seiton.py
@@ -16,15 +16,18 @@ class Connect(Node):
         self.publisher = self.create_publisher(Bool, 'emergency', 100)
         # self.port = serial.tools.list_ports.comports()[1].device
         self.port = '/dev/seiton'
+        # self.port = '/dev/ttyACM7'
         print(self.port)
-        self.uart = serial.Serial(self.port, 115200)
+        self.uart = serial.Serial(self.port, 115200,write_timeout=2)
         self.tmr = self.create_timer(0.01, self.callback)
         self.uart.write("s\n".encode('ascii'))
         self.pos = 0
         self.prev = 0
         self.flag = False
+        self.g_flag = False
 
     def joy_callback(self, joy_msg):
+        self.get_logger().info('callback')
         # if joy_msg.axes[6] == -1: # right
         #     self.pos = 0
         # elif joy_msg.axes[6] == 1: # left
@@ -44,6 +47,15 @@ class Connect(Node):
         elif joy_msg.axes[2] < 0.5:
             self.pos = 4
             self.flag = True
+        elif joy_msg.axes[2] == 1 and self.flag:
+            self.pos = 7
+            self.flag =False
+        elif joy_msg.axes[5] < 0.5:
+            self.pos = 5
+            self.g_flag = True
+        elif joy_msg.axes[5] == 1 and self.g_flag:
+            self.pos = 6
+            self.g_flag = False
         if joy_msg.buttons[8]:
             msg = Bool()
             msg.data = True
@@ -51,10 +63,11 @@ class Connect(Node):
 
 
     def send(self):
-        if self.pos != self.prev or self.flag:
+        if self.pos != self.prev:
             message = str(self.pos)+'\n'
             self.get_logger().info('Sent: '+str(self.pos))
             self.uart.write(message.encode('ascii'))
+            self.get_logger().info('fin')
             self.flag = False
         self.prev = self.pos
 
